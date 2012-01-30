ae1fa1c2103f505a45bbb8219f9b4e6415247959
Kyle Conroy <kyle.j.conroy@gmail.com>
2012-01-29 20:49:02 -0800

    Use gunicorn

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import urllib
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -15,6 +16,7 @@ def index():
     return render_template("index.html", source="http://www.tired.com", text="")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
